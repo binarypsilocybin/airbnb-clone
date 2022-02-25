1861aa7269d5fe0f438a69c7d1ec2e18c02bcccb
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-02-25 23:37:27 +0000

    Project training airbnb clone

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logoImage from "../images/airbnb-logo.png"
+
+export default function Navbar(){
+    return (
+        <div className="navbar">
+            <img src={logoImage} width={82} />
+        </div>
+    )
+
+}
