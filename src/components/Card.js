1c98627aa9384417ffea8f02c16f3ca90a41fee5
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-03-01 16:29:04 +0000

    new badge

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,10 @@
 import React from "react"
 
 export default function Card(props) {
+     
     return (
         <div className="card">
+            {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
             <img src={require(`../images/${props.img}`)} className="card--image" alt="" />
             <div className="card--stats">
                 <img src={require(`../images/star.png`)} className="card--star" alt="" />
