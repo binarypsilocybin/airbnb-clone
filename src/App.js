ea19c22a46804f9d0ee9dc24de2901e40fb9547e
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-03-01 16:29:17 +0000

    New badge

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import data from "./data"
 import "./style.css"
 
 /*
@@ -9,19 +10,31 @@ Challenge: Build the Navbar component.
 Check the Figma file for the design specifics.
 */
 
+
 export default function App() {
-    return (
-      <div className="container">
-        <Navbar />
-        <Hero />
-        <Card 
-          img="image-katie.png"
-          rating="5.0"
-          reviewCount={6}
-          country="USA"
-          title="Life Lessons with Katie Zaferes"
-          price={136}
-        />
-      </div>
-    )
+  // <Hero />
+const cards = data.map(item => {
+return (
+  <Card 
+      key={item.id}
+      img={item.coverImg}
+      rating={item.stats.rating}
+      reviewCount={item.stats.reviewCount}
+      location={item.location}
+      title={item.title}
+      price={item.price}
+      openSpots={item.openSpots}
+  />
+)
+})        
+
+return (
+<div>
+  <Navbar />
+  <Hero />
+  <section className="cards-list">
+    {cards}
+  </section>
+</div>
+)
 }
